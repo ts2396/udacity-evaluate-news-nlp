b9ca69b1e97bc210b226d7786ff90bba080a9504
ts2396 <48188551+ts2396@users.noreply.github.com>
2020-04-22 19:46:21 -0400

    commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,31 +1,36 @@
-console.log("I exist still, I am the  Article");
+console.log('I exist still, I am the  Article');
 
 function handleSubmit(event) {
-    event.preventDefault()
-    const url = document.querySelectorAll('input[name=url]')
-    let userUrl = document.getElementById('url').value;
-    if (Client.checkURL(userUrl)) {
-        fetch('http://localhost:8081/eval', {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({url: url[0].value})
-        })
-            .then(res => res.json())
-            .then(function (res) {
-                document.querySelector('section.url-results #polarity').innerHTML = res.polarity
-                document.querySelector('section.url-results #subjectivity').innerHTML = res.subjectivity
-                document.querySelector('section.url-results #polarity_confidence').innerHTML = res.polarity_confidence
-                document.querySelector('section.url-results #subjectivity_confidence').innerHTML = res.subjectivity_confidence
-                document.querySelector('section.url-results #excerpt').innerHTML = res.text
-            })
-    } else {
-        var url_check = document.querySelector('section.url_check');
-        var wrong = document.querySelector('section.url_check #wrong');
-        wrong.innerHTML = "The URL you entered:\"" + JSON.stringify(url[0].value) + "\" is not correct. Please check your URL"
-        url_check.style.display = "block";
+  event.preventDefault();
+  const url = document.querySelectorAll('input[name=url]');
+  let userUrl = document.getElementById('url').value;
+  if (Client.checkURL(userUrl)) {
+    fetch('http://localhost:8081/eval', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ url: url[0].value }),
+    })
+      .then((res) => res.json())
+      .then(function (res) {
+        document.querySelector('section.url-results #polarity').innerHTML =
+          res.polarity;
+        document.querySelector('section.url-results #subjectivity').innerHTML =
+          res.subjectivity;
+        document.querySelector(
+          'section.url-results #polarity_confidence'
+        ).innerHTML = res.polarity_confidence;
+        document.querySelector(
+          'section.url-results #subjectivity_confidence'
+        ).innerHTML = res.subjectivity_confidence;
+        document.querySelector('section.url-results #excerpt').innerHTML =
+          res.text;
+        });
+      } else {
+        console.log('Url not valid');
+      }
     }
-}
-export {handleSubmit}
+    
+    export { handleSubmit };
